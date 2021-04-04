78fb08c8003026c9cc7a79071b2deb33a4a0c2aa
Rahuls-iMac <rahwinside@gmail.com>
2021-04-05 00:22:00 +0530

    Login API

--- routes/auth.py
+++ routes/auth.py
@@ -1,18 +1,13 @@
 from db_config import mysql
+import hashlib
 
 
 def authenticate_email(email, password):
+    password = hashlib.sha512(password).digest()
     sql = f"SELECT users.userID, users.emailID, users.fullName, users.token, users.domain, users.rollNo, users.department, users.bloodType, users.phone, users.emergencyContact, users.emergencyContactName, users.emergencyContactRelation, users.lastSignInTimeStamp, users.signUpTimeStamp, domains.organizationName, domains.email, domains.phone, domains.address, FROM users, domains WHERE users.domain = domains.domain AND email = '{email}' AND password = '{password}'"
     cnx = mysql.connect()
     cursor = cnx.cursor()
     cursor.execute(sql)
     for row in cursor:
-        for iter in row:
-            if iter is None:
-                is_complete = "false"
-                break
-
-    json_dict = {"auth-status": "true", "isProfileComplete": is_complete}
-    res = jsonify(json_dict)
-    res.status_code = 200
-    return res
+        return row
+    return False
--- routes/login.py
+++ routes/login.py
@@ -1,12 +1,11 @@
-import pymysql
+from app import app, forbidden, internal_server_error
+from flask import request
+from routes.auth import authenticate_email
 
-from app import app, forbidden, not_found
-from flask import jsonify, request
-from db_config import mysql
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Login API, returns status=true/false, isComplete=true/false
+    # Login API, returns dict/403/500
     try:
         _email = request.form['email']
         _password = request.form['password']
@@ -14,41 +13,15 @@ def login():
         if _email and _password and request.method == 'POST':
             var = authenticate_email(_email, _password)
 
-            if var == "true":
-                # Check with db if record is complete
-                is_complete = "true"
-                sqlQuery = f"SELECT firstName, lastName, areaCode, phoneNumber, postalCode, email, gender, pacemaker, iud, pregnant, allergies, surgeries FROM `users` WHERE uid = '{_uid}'"
-                cnx = mysql.connect()
-                cursor = cnx.cursor()
-                cursor.execute(sqlQuery)
-                is_complete = "false"
-                for row in cursor:
-                    is_complete = "true"
-                    for iter in row:
-                        if iter is None:
-                            is_complete = "false"
-                            break
-
-                json_dict = {"auth-status": "true", "isProfileComplete": is_complete}
-                res = jsonify(json_dict)
+            if not var:
+                res = var
                 res.status_code = 200
                 return res
 
-            elif var == "invalid-data":
-                json_dict = {"auth-status": "invalid-data", "isProfileComplete": "false"}
-                res = jsonify(json_dict)
-                res.status_code = 403
-                return res
-
+            # If POST is empty
             else:
-                json_dict = {"auth-status": "false", "isProfileComplete": "false"}
-                res = jsonify(json_dict)
-                res.status_code = 200
-                return res
-        # If POST is empty
-        else:
-            return forbidden()
+                return forbidden()
 
     except Exception as e:
         print(e)
-        return forbidden()
+        return internal_server_error()
