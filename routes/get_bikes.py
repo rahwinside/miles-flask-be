a2745a4c318e411280cf4f596d2980d2dcb1b7ff
Rahuls-iMac <rahwinside@gmail.com>
2021-04-06 12:47:11 +0530

    Get available bike count API

--- routes/get_bikes.py
+++ routes/get_bikes.py
@@ -19,10 +19,47 @@ def get_bikes():
             var = authenticate_email_token(_email, _token)
 
             if var:
-                # Write query here
                 sql = f"SELECT bikes.bikeID, bikes.currentStationID, bikes.status, stations.stationName FROM bikes, " \
                       f"stations, users WHERE users.email = '{_email}' AND  bikes.currentStationID =  " \
                       f"stations.stationID AND stations.domain = users.domain "
+                cnx = mysql.connect()
+                cursor = cnx.cursor()
+                cursor.execute(sql)
+                bike_list = []
+                for row in cursor:
+                    bike_list.append(row)
+
+                res = jsonify(bike_list)
+                res.status_code = 200
+                return res
+
+            # If POST is empty
+            else:
+                return forbidden()
+
+    except Exception as e:
+        print(e)
+        return internal_server_error()
+
+
+@app.route('/get-avail-bikes', methods=['POST'])
+def get_bikes_avail_count():
+    try:
+        try:
+            _email = request.form['email']
+            _token = request.form['token']
+        except Exception as e:
+            print(e)
+            return bad_request()
+
+        if _email and _token and request.method == 'POST':
+            var = authenticate_email_token(_email, _token)
+
+            if var:
+                sql = f"SELECT stations.stationName, stations.stationID, count(bikes.bikeID) AS available FROM bikes, " \
+                      f"stations, users WHERE users.email = '{_email}' AND bikes.currentStationID = " \
+                      f"stations.stationID AND stations.domain = users.domain AND bikes.status = 'free' GROUP BY " \
+                      f"stations.stationName, stations.stationID "
                 cnx = mysql.connect()
                 cursor = cnx.cursor()
                 cursor.execute(sql)
